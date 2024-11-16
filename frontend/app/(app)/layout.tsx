0c3fe810aaf165bd5caee691d5364d3ea1762f21
syjn99 <syjn99@gmail.com>
2024-11-17 03:23:49 +0700

    Fix onboarding design

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -5,7 +5,7 @@ import Providers from "./providers";
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <Providers>
-      <div className="min-h-screen flex flex-col bg-gradient-to-r from-[#E0EAFC] to-[#CFDEF3]">
+      <div className="min-h-screen flex flex-col">
         <Header />
         <main className="flex-1 pt-16">{children}</main>
         <Footer />
